187a7b241dae0b12c473bb3a0b314e1fdbefeeb7
DanielJM96 <daniel_juar_mtz96@outlook.es>
2023-05-01 22:11:52 -0600

    primercommit

---
+++ app.js
@@ -0,0 +1,35 @@
+
+
+// let numero1= 3; 
+// let numero2= 5; 
+// let resultado;
+
+// resultado=(numero1+numero2)*2;
+// console.log(resultado);
+
+// //para convertir a mayusculas 
+// let texto ="esto es un texto"
+
+// texto= texto.toUpperCase();
+
+// // para convertir a minusculas
+// texto= texto.toLowerCase();
+
+// //longitud de string 
+// let oracionLongitud= "Carlos es como Granados";
+
+// //oracionLongitud.split()
+// console.log(oracionLongitud.split(" "));
+
+// let prueba= "Carlos es como Granados";
+// console.log(prueba.length);
+
+
+
+
+
+let sabores = ["chocolate","vainilla","fresa"];
+
+for(let i= 0; i<=2;i++){
+    console.log(`El sabor es: ${sabores[i]}`)
+}
